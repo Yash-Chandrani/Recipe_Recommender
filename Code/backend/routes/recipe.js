7dc4b8004a88049546c4ad21766112fd1cbe7209
Ashaka Mehta <ashakamehta8@gmail.com>
2023-11-27 20:45:23 -0500

    route endpoint to get recipes by id list

--- Code/backend/routes/recipe.js
+++ Code/backend/routes/recipe.js
@@ -3,10 +3,12 @@ const {
   apiGetRecipeCuisines,
   apiGetRecipes,
   apiPostRecipes,
+  apiGetRecipesById,
 } = require("../controllers/recipe");
 const getRecipe = express.Router();
 const getCuisine = express.Router();
 const postRecipe = express.Router();
+const getRecipesById = express.Router();
 
 //URl to get the recipes
 getRecipe.route("/").get(apiGetRecipes);
@@ -15,8 +17,11 @@ postRecipe.route("/").post(apiPostRecipes);
 
 getCuisine.route("/").get(apiGetRecipeCuisines);
 
+getRecipesById.route("/").post(apiGetRecipesById);
+
 module.exports = {
   getRecipe,
   postRecipe,
   getCuisine,
+  getRecipesById
 };
